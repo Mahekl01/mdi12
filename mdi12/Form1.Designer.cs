a74ba260e1aacb7bbc47b99e9718c062ad28e26a
mahek <mahek@LAPTOP-EQSSOR0C>
2025-05-09 17:38:44 +0530

    Add project files.

---
+++ mdi12/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿using System.Drawing;
+
+namespace mdi12
+{
+    partial class MDIParent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnContacts = new System.Windows.Forms.Button();
+            this.btnSMS = new System.Windows.Forms.Button();
+            this.btnCalls = new System.Windows.Forms.Button();
+            this.btnDeviceInfo = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnContacts
+            // 
+            this.btnContacts.Location = new System.Drawing.Point(84, 97);
+            this.btnContacts.Name = "btnContacts";
+            this.btnContacts.Size = new System.Drawing.Size(75, 23);
+            this.btnContacts.TabIndex = 0;
+            this.btnContacts.Text = "contacts";
+            this.btnContacts.UseVisualStyleBackColor = true;
+            this.btnContacts.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSMS
+            // 
+            this.btnSMS.Location = new System.Drawing.Point(237, 97);
+            this.btnSMS.Name = "btnSMS";
+            this.btnSMS.Size = new System.Drawing.Size(75, 23);
+            this.btnSMS.TabIndex = 1;
+            this.btnSMS.Text = "SMS";
+            this.btnSMS.UseVisualStyleBackColor = true;
+            // 
+            // btnCalls
+            // 
+            this.btnCalls.Location = new System.Drawing.Point(411, 97);
+            this.btnCalls.Name = "btnCalls";
+            this.btnCalls.Size = new System.Drawing.Size(75, 23);
+            this.btnCalls.TabIndex = 2;
+            this.btnCalls.Text = "call logs";
+            this.btnCalls.UseVisualStyleBackColor = true;
+            // 
+            // btnDeviceInfo
+            // 
+            this.btnDeviceInfo.Location = new System.Drawing.Point(599, 97);
+            this.btnDeviceInfo.Name = "btnDeviceInfo";
+            this.btnDeviceInfo.Size = new System.Drawing.Size(75, 23);
+            this.btnDeviceInfo.TabIndex = 3;
+            this.btnDeviceInfo.Text = "device info";
+            this.btnDeviceInfo.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(140, 286);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Load data ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(350, 286);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "save to database";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(535, 286);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "report";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // MDIParent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnDeviceInfo);
+            this.Controls.Add(this.btnCalls);
+            this.Controls.Add(this.btnSMS);
+            this.Controls.Add(this.btnContacts);
+            this.Name = "MDIParent";
+            this.Text = "MDIParent";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnContacts;
+        private System.Windows.Forms.Button btnSMS;
+        private System.Windows.Forms.Button btnCalls;
+        private System.Windows.Forms.Button btnDeviceInfo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ mdi12/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mdi12
+{
+    public partial class MDIParent : Form
+    {
+        public MDIParent()
+        {
+            
+            this.IsMdiContainer = true;
+        }
+
+        private void ShowChildForm(Form childForm)
+        {
+            foreach (Form f in this.MdiChildren)
+            {
+                f.Close(); // Close any open children
+            }
+
+            childForm.MdiParent = this;
+            childForm.WindowState = FormWindowState.Maximized;
+            childForm.Show();
+        }
+
+        private void btnContacts_Click(object sender, EventArgs e)
+        {
+            ShowChildForm(new ContactsForm());
+        }
+
+        private void btnSMS_Click(object sender, EventArgs e)
+        {
+            ShowChildForm(new SMSForm());
+        }
+
+        private void btnCalls_Click(object sender, EventArgs e)
+        {
+            ShowChildForm(new CallLogsForm());
+        }
+
+        private void btnDeviceInfo_Click(object sender, EventArgs e)
+        {
+            ShowChildForm(new DeviceInfoForm());
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+    public partial class ContactsForm : Form
+    {
+        private DataGridView dataGridView1;
+
+        public ContactsForm()
+        {
+            InitializeComponent();
+            SetupGrid();
+        }
+        private void InitializeComponent()
+        {
+            this.Text = "Contacts";
+            this.Size = new Size(600, 400);
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+        private void SetupGrid()
+        {
+            dataGridView1 = new DataGridView();
+            dataGridView1.Dock = DockStyle.Fill;
+            this.Controls.Add(dataGridView1);
+        }
+
+        public void LoadData()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Phone Number");
+
+            dt.Rows.Add("Alice", "+1234567890");
+            dt.Rows.Add("Bob", "+9876543210");
+
+            dataGridView1.DataSource = dt;
+        }
+
+        public void SaveToDatabase()
+        {
+            MessageBox.Show("Contacts saved to database (mock).");
+        }
+
+        public void ViewReport()
+        {
+            MessageBox.Show("Contacts report generated (mock).");
+        }
+    }
+
+    public partial class SMSForm : Form
+    {
+        private DataGridView dataGridView1;
+
+        public SMSForm()
+        {
+            InitializeComponent();
+            SetupGrid();
+        }
+        private void InitializeComponent()
+        {
+            this.Text = "SMS Messages";
+            this.Size = new Size(600, 400);
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+        private void SetupGrid()
+        {
+            dataGridView1 = new DataGridView();
+            dataGridView1.Dock = DockStyle.Fill;
+            this.Controls.Add(dataGridView1);
+        }
+
+        public void LoadData()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Sender");
+            dt.Columns.Add("Message");
+
+            dt.Rows.Add("John", "Hi, how are you?");
+            dt.Rows.Add("Bank", "Your OTP is 123456");
+
+            dataGridView1.DataSource = dt;
+        }
+
+        public void SaveToDatabase()
+        {
+            MessageBox.Show("SMS messages saved to database (mock).");
+        }
+
+        public void ViewReport()
+        {
+            MessageBox.Show("SMS report generated (mock).");
+        }
+    }
+
+
+    public partial class CallLogsForm : Form
+    {
+        private DataGridView dataGridView1;
+
+        public CallLogsForm()
+        {
+            InitializeComponent();
+            SetupGrid();
+        }
+        private void InitializeComponent()
+        {
+            this.Text = "Call Logs";
+            this.Size = new Size(600, 400);
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+        private void SetupGrid()
+        {
+            dataGridView1 = new DataGridView();
+            dataGridView1.Dock = DockStyle.Fill;
+            this.Controls.Add(dataGridView1);
+        }
+
+        public void LoadData()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Caller");
+            dt.Columns.Add("Type");
+            dt.Columns.Add("Time");
+
+            dt.Rows.Add("Alice", "Missed", "10:30 AM");
+            dt.Rows.Add("Bob", "Received", "11:45 AM");
+
+            dataGridView1.DataSource = dt;
+        }
+
+        public void SaveToDatabase()
+        {
+            MessageBox.Show("Call logs saved to database (mock).");
+        }
+
+        public void ViewReport()
+        {
+            MessageBox.Show("Call logs report generated (mock).");
+        }
+    }
+
+    public partial class DeviceInfoForm : Form
+    {
+        private DataGridView dataGridView1; // Declare the DataGridView
+
+
+
+        private void InitializeComponent()
+        {
+            this.Text = "Device Info";
+            this.Size = new Size(600, 400);
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+
+
+        private void SetupGrid()
+        {
+            dataGridView1 = new DataGridView();
+            dataGridView1.Dock = DockStyle.Fill;
+            this.Controls.Add(dataGridView1);
+        }
+
+        public void LoadData()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Property");
+            dt.Columns.Add("Value");
+
+            dt.Rows.Add("Model", "Samsung Galaxy S21");
+            dt.Rows.Add("OS", "Android 13");
+            dt.Rows.Add("Battery", "80%");
+
+            dataGridView1.DataSource = dt;
+        }
+       
+        
+            
+    }
+
+
+}
